ed97edb70db9d40cef863496a126d4bbdf8cffba
Lup Gabriel <gwolf2u.website@gmail.com>
2022-02-18 17:30:07 +0530

    sm8150-common: drop snap
    Signed-off-by: Shantanu Sarkar <shantanuplussarkar@gmail.com>

--- common.mk
+++ common.mk
@@ -163,7 +163,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:64 \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    Snap \
     vendor.oneplus.hardware.camera@1.0.vendor:64 \
     vendor.qti.hardware.camera.device@1.0.vendor:64
 
